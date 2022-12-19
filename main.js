9a483806c177a70a447c045a810bf0950032c581
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-19 07:28:56 -0800

    index

---
+++ main.js
@@ -0,0 +1,21 @@
+let fruit=["apple","orange","mango","bannana","lemon"];
+
+let a =parseInt(prompt("enter the name"));
+let c=prompt("enter the name");
+fruit[a]=c
+
+console.log(fruit)
+document.write(fruit)
+
+
+/*for(i=0; i<=a; i++){
+
+  fruit[i]=c
+      
+      
+}*/
+
+
+
+
+
